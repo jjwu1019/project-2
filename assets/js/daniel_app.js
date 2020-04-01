616121154766acb64816639145581d6f02771465
jjwu1019 <wuwuwuuu@gmail.com>
2020-04-01 14:41:31 -0400

    4/1/2020 add highchart

--- assets/js/daniel_app.js
+++ assets/js/daniel_app.js
@@ -47,7 +47,7 @@ function buildTable(b){
         var calories =  selected_region.map(y => y.calories)
 
 
-        var tbody = d3.select("#food-table").select("tbody");
+        var tbody = d3.select("#datatable").select("tbody");
         tbody.html("");
        
     
@@ -61,7 +61,7 @@ function buildTable(b){
             
         }
     
-
+        buildHighChart()
     }).catch(function(error) {
         console.log(error);
       });
@@ -76,6 +76,30 @@ function buildTable(b){
 
    function optionChanged(b){
         buildTable(b)
-       
-
    };
+
+   function buildHighChart() {
+    Highcharts.chart('container', {
+      data: {
+          table: 'datatable'
+      },
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Data extracted from a HTML table in the page'
+      },
+      yAxis: {
+          allowDecimals: false,
+          title: {
+              text: 'Units'
+          }
+      },
+      tooltip: {
+          formatter: function () {
+              return '<b>' + this.series.name + '</b><br/>' +
+                  this.point.y + ' ' + this.point.name.toLowerCase();
+          }
+      }
+  });
+   }
